8c36b052084264edfba5b3e59cdbe494abc2129e
laynia <vc_mikuu@hotmail.com>
2024-08-17 17:13:31 +0200

    Adding config

---
+++ include/config.hpp
@@ -0,0 +1,19 @@
+#pragma once
+
+#include "config-utils/shared/config-utils.hpp"
+#include "UnityEngine/Color.hpp"
+
+// Mhm..
+
+DECLARE_CONFIG(ModConfig,
+    CONFIG_VALUE(Rainbow, bool, "Rainbow Bar when full energy", true);
+    CONFIG_VALUE(AlwaysRainbow, bool, "Always Rainbow Bar", false);
+    CONFIG_VALUE(Fadeout, bool, "Fadeout", false);
+    CONFIG_VALUE(FullFade, bool, "Use fade between 15% and 95% colors", false);
+    CONFIG_VALUE(Alpha, float, "Alpha", 1.0);
+    CONFIG_VALUE(Diehp, UnityEngine::Color, "under 15% energy", ConfigUtils::Color(1.0f, 0.0f, 0.0f, 0.0f));
+    CONFIG_VALUE(Lowhp, UnityEngine::Color, "under 50% energy", ConfigUtils::Color(1.0f, 0.0f, 0.0f, 0.0f));
+    CONFIG_VALUE(Midhp, UnityEngine::Color, "over 70% energy", ConfigUtils::Color(0.0f, 1.0f, 0.0f, 0.0f));
+    CONFIG_VALUE(Highhp, UnityEngine::Color, "over 95% energy", ConfigUtils::Color(0.0f, 1.0f, 1.0f, 0.0f));
+    CONFIG_VALUE(Defhp, UnityEngine::Color, "over 50% energy", ConfigUtils::Color(1.0f, 1.0f, 1.0f, 0.0f));
+)
--- include/main.hpp
+++ include/main.hpp
@@ -11,14 +11,33 @@
 #include "beatsaber-hook/shared/utils/hooking.hpp"
 #include "beatsaber-hook/shared/utils/il2cpp-functions.hpp"
 #include "beatsaber-hook/shared/utils/logging.hpp"
+#include "HMUI/ViewController.hpp"
+#include "HMUI/Touchable.hpp"
+#include "bsml/shared/BSML.hpp"
 
+#include "custom-types/shared/macros.hpp"
 
+#include "scotland2/shared/loader.hpp"
 #include "paper/shared/logger.hpp"
 
 #include "_config.hpp"
+#include "config.hpp"
+
+void DidActivate(HMUI::ViewController* self, bool firstActivation, bool addedToHierarchy, bool screenSystemEnabling);
 
 // Define these functions here so that we can easily read configuration and
 // log information from other files
 Configuration &getConfig();
 
-constexpr auto PaperLogger = Paper::ConstLoggerContext("redbar");
+modloader::ModInfo modInfo{MOD_ID, VERSION, 0};
+
+/// @brief A logger, useful for printing debug messages
+/// @return
+static constexpr auto Logger = Paper::ConstLoggerContext(MOD_ID "_" VERSION);
+
+#define MOD_EXPORT __attribute__((visibility("default")))
+#ifdef __cplusplus
+#define MOD_EXPORT_FUNC extern "C" MOD_EXPORT
+#else
+#define MOD_EXPORT_FUNC MOD_EXPORT
+#endif
